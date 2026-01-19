247b42ade0d32d7d3ad2e57df70a252b927b8e44
supr1yo <45136370+supriyo@users.noreply.github.com>
2026-01-19 12:58:39 +0530

    Arrays

---
+++ basics/Arrays.java
@@ -0,0 +1,9 @@
+public class Arrays {
+    public static void main(String[] args) {
+        String[] names = {"Ryan", "Reno", "Rob", "Ruddy", "Ruby"};
+       
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
